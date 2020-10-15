f81e079292410d12217fedeea73af547f87e2d13
Conner Mayfield <nauchten@Conners-MacBook-Pro.local>
2020-10-15 18:41:47 -0400

    sending code to github, time 6:41pm

--- src/my-angular-project/src/app/app.module.ts
+++ src/my-angular-project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule, // ask Professor Sexton about error // https://www.youtube.com/watch?v=RSJxWJ6dCL4 you were using this tutorial at youtube point 5:57
+    AppRoutingModule  // you were using this to connect backend to frontend and display the data
   ],
   providers: [],
   bootstrap: [AppComponent]
