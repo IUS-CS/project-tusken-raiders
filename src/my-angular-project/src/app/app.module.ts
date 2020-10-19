1e14471dc59dbdc973431b79f582d474c213dfe7
Conner Mayfield <nauchten@Conners-MBP.lan1>
2020-10-18 23:19:15 -0400

    sending code to github and added textbox to website

--- src/my-angular-project/src/app/app.module.ts
+++ src/my-angular-project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from "@angular/common/http";
+//import { HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule, // ask Professor Sexton about error // https://www.youtube.com/watch?v=RSJxWJ6dCL4 you were using this tutorial at youtube point 5:57
+    //HttpClientModule, // ask Professor Sexton about error // https://www.youtube.com/watch?v=RSJxWJ6dCL4 you were using this tutorial at youtube point 5:57
     AppRoutingModule  // you were using this to connect backend to frontend and display the data
   ],
   providers: [],
